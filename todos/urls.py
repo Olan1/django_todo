3bd0d64727bde2daaa4becb23c32ca9a75fd4b61
Olan <olanbarron@gmail.com>
2022-05-24 20:33:09 +0100

    Added full CRUD functionality

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('edit/editrecord/<int:id>', views.editrecord, name='editrecord'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import ToDoItem
+
 
 def index(request):
     todos = ToDoItem.objects.all().values()
@@ -10,3 +12,41 @@ def index(request):
         'todos': todos,
     }
     return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    item = request.POST['item']
+    urgent = request.POST.get('urgent') == 'on'
+    todo = ToDoItem(item=item, urgent=urgent)
+    todo.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    todo = ToDoItem.objects.get(id=id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def edit(request, id):
+    todo = ToDoItem.objects.get(id=id)
+    template = loader.get_template('edit.html')
+    context = {
+        'todo': todo
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def editrecord(request, id):
+    item = request.POST['item']
+    urgent = request.POST.get('urgent') == 'on'
+    todo = ToDoItem.objects.get(id=id)
+    todo.item = item
+    todo.urgent = urgent
+    todo.save()
+    return HttpResponseRedirect(reverse('index'))

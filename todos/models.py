4f2d6248d280be1c511148ffa992863176a9f495
Olan <olanbarron@gmail.com>
2022-05-24 01:04:21 +0100

    Added todo list and create todo form

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class ToDoItem(models.Model):
+    item = models.CharField(max_length=255)
+    urgent = models.BooleanField(default=False)
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import ToDoItem
 
 def index(request):
-    return HttpResponse("Hello world!")
+    todos = ToDoItem.objects.all().values()
+    template = loader.get_template('todolist.html')
+    context = {
+        'todos': todos,
+    }
+    return HttpResponse(template.render(context, request))
